cf92ad4dcc9599ce64bcc5af2b5fb4cf9b833435
Julia Mayumi <julia.mayumi@creditas.com.br>
2022-07-29 09:30:20 -0300

    Add more prints, and modify rectangle in main function

--- calculo-retangulo/src/main/kotlin/main.kt
+++ calculo-retangulo/src/main/kotlin/main.kt
@@ -1,14 +1,22 @@
 fun main(){
     val rectangle = mutableMapOf("Width" to 10, "Height" to 10, "Color" to "Red");
 
+    println("Primeira Área calculada: ${calcArea(rectangle)}");
     println(toString(rectangle));
-    print("A area do retangulo é: ${calcArea(rectangle)}");
+
+    rectangle["Width"] = 20;
+    println("\nSegunda Área calculada: ${calcArea(rectangle)}");
+    println(toString(rectangle));
+
+    rectangle["Height"] = 20;
+    println("\nTerceira Área calculada: ${calcArea(rectangle)}");
+    println(toString(rectangle));
 }
+
 // a função toString irá retornar os atributos do retangulo
 // o Map server como um buscador de parametros de que seu desejo extrair
 fun toString(shape: Map<String, Any>): String{
-    return "Width = ${shape["Width"]},\nHeight = ${shape["Height"]},\ncolor = ${shape["Color"]}"
-
+    return "Width = ${shape["Width"]}, Height = ${shape["Height"]}, Color = ${shape["Color"]}, Área = ${calcArea(shape)}"
 }
 
 fun calcArea(shape : Map<String, Any>) : Int{
